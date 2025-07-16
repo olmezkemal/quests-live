2ac053463c99ee4b6f8321a2766175b99ae0de33
Kemal Olmez <olmez@filmcinecoin.com>
2025-07-16 13:53:05 +0300

    Updated quests.json identifiers

--- models/Quest.js
+++ models/Quest.js
@@ -1,11 +1,38 @@
-
-const mongoose = require("mongoose");
-
-const QuestSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  questId: { type: String, required: true },
-  completedAt: { type: Date, default: Date.now },
-  reward: { type: Number, required: true }
-});
-
-module.exports = mongoose.model("Quest", QuestSchema);
+{
+  "daily-login": {
+    "title": "Daily Login",
+    "reward": 25,
+    "type": "daily",
+    "description": "Earn 25 PASS every day just by visiting the site."
+  },
+  "follow-twitter": {
+    "title": "Follow on Twitter",
+    "reward": 1000,
+    "type": "social",
+    "description": "Follow @filmcinecoin on Twitter and support our growing community."
+  },
+  "subscribe-youtube": {
+    "title": "Subscribe to YouTube",
+    "reward": 1000,
+    "type": "social",
+    "description": "Subscribe to our official YouTube channel and stay updated with new releases."
+  },
+  "buy-nft": {
+    "title": "Buy an NFT",
+    "reward": 50,
+    "type": "ecosystem",
+    "description": "Buy any NFT from the marketplace and receive rewards."
+  },
+  "stake-filmcc": {
+    "title": "Stake FilmCC",
+    "reward": 50,
+    "type": "ecosystem",
+    "description": "Stake your FilmCC tokens and earn extra PASS."
+  },
+  "submit-film-idea": {
+    "title": "Submit a Film Idea",
+    "reward": 1000,
+    "type": "creative",
+    "description": "Share your film idea and be part of the next funded project."
+  }
+}
